1294ad3c1928d189a540803bece9854d80107acc
Shohmasud <name@mail>
2021-07-18 04:38:41 +0700

    Уставновка модулей

--- ApiRestService/views.py
+++ ApiRestService/views.py
@@ -1,2 +1,10 @@
 from django.shortcuts import render
 # Create your views here.
+import csv,os,json
+from rest_framework import status
+from psycopg2 import IntegrityError
+from .models import response,fileName
+from  rest_framework.views import APIView
+from django.shortcuts import render, redirect
+from  rest_framework.response import Response
+from django.http import HttpResponse, HttpResponseRedirect
