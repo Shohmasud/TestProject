fa481dba553df156cf14d22b2e19a439218bbe39
Shohmasud <name@mail>
2021-07-18 04:08:04 +0700

    Регистрация классов модели в admin

--- ApiRestService/admin.py
+++ ApiRestService/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+
+class File_Name(admin.ModelAdmin):
+    list_display = ('id', 'name_file')
+    search_fields = ('id', 'name_file')
+admin.site.register(fileName, File_Name)
+
+
+
+class Response(admin.ModelAdmin):
+    list_display = ('id','username','spent_money', 'gems')
+    search_fields = ('id','username','spent_money', 'gems')
+admin.site.register(response, Response)
